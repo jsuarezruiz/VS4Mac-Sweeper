38ba043182684cdecd53f2023219e260d11b32b3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-12-27 12:26:00 +0100

    Initial code

---
+++ src/VS4Mac.Sweeper/VS4Mac.Sweeper/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "VS4Mac.Sweeper",
+    Namespace = "VS4Mac.Sweeper",
+    Version = "0.1"
+)]
+
+[assembly: AddinName("VS4Mac.Sweeper")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("VS4Mac.Sweeper")]
+[assembly: AddinAuthor("Javier Suárez Ruiz")]
